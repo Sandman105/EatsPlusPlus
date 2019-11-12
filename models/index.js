49d39ef0291c5868e52165bc99b77c2fe99e8d0d
Varun Master <varunmaster95@gmail.com>
2019-11-12 17:08:54 -0500

    changed jawsdb url to index.js model

--- models/index.js
+++ models/index.js
@@ -8,9 +8,13 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
+if (process.env.JAWSDB_URL) {
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+// if (config.use_env_variable) {
+//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
+// } 
+else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
