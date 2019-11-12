a3514fb46e93f4cc49399ddcdd7e234ffe7bcd8c
Varun Master <varunmaster95@gmail.com>
2019-11-12 17:13:44 -0500

    final changes to app for deployment to heroku

--- models/index.js
+++ models/index.js
@@ -11,24 +11,24 @@ var db        = {};
 if (process.env.JAWSDB_URL) {
   var sequelize = new Sequelize(process.env.JAWSDB_URL);
 }
-// if (config.use_env_variable) {
-//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } 
+else if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+}
 else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
   .readdirSync(__dirname)
-  .filter(function(file) {
+  .filter(function (file) {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach(function(file) {
+  .forEach(function (file) {
     var model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function(modelName) {
+Object.keys(db).forEach(function (modelName) {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
